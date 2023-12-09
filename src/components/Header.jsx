f95cab974757d750be79f12cbaee3834c6ebe685
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2023-12-09 21:04:45 +0300

    add improvments to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { BiSolidBriefcaseAlt2 } from "react-icons/bi";
+import { MdClose } from "react-icons/md";
+import { useState } from "react";
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <header className="flex items-center justify-around p-5 max-md:justify-between sticky z-50 top-0 bg-white border-b">
@@ -24,9 +27,35 @@ const Header = () => {
           <button className="border px-4 py-1 border-gray-200 rounded-md font-light text-md hover:bg-green hover:text-white duration-300 max-[400px]:hidden">
             Sign up
           </button>
-          <GiHamburgerMenu className="text-3xl hover:text-green md:hidden" />
+
+          {!open ? (
+            <GiHamburgerMenu
+              onClick={() => {
+                setOpen(!open);
+              }}
+              className="text-3xl hover:text-green md:hidden"
+            />
+          ) : (
+            <MdClose
+              onClick={() => {
+                setOpen(!open);
+              }}
+              className="text-3xl hover:text-green md:hidden"
+            />
+          )}
         </div>
       </header>
+      <ul
+        className={`${
+          open ? "open" : "close"
+        } flex flex-col items-center justify-center sticky top-0 bg-white py-6 gap-6  nav-links md:hidden`}
+      >
+        <li>Jobs</li>
+        <li>Membership</li>
+        <li>Newsletter</li>
+        <li>Freelancers</li>
+        <li>Blog</li>
+      </ul>
     </>
   );
 };
